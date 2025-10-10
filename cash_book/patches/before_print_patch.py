11fa1c99ee58a3d2d623b154422f85a1e52337f1
munyaradzi <munyaradzichirove@gmail.com>
2025-10-10 14:29:11 +0200

    print

--- cash_book/patches/before_print_patch.py
+++ cash_book/patches/before_print_patch.py
@@ -1,11 +1,10 @@
 import frappe
-from frappe.utils.safe_exec import safe_exec
 
 def safe_before_print(doc, method=None, settings=None):
-    frappe.msgprint("print check")
-
     try:
-        # Only patch these doctypes
+        # Debug log instead of msgprint
+        frappe.msgprint("check print now")
+
         if doc.doctype in [
             "Purchase Order",
             "Sales Order",
@@ -20,13 +19,13 @@ def safe_before_print(doc, method=None, settings=None):
                 doc.group_similar_items()
 
             df = doc.meta.get_field("discount_amount")
-            if doc.get("discount_amount") and hasattr(doc, "taxes") and not len(doc.taxes):
-                df.set("print_hide", 0)
-                doc.discount_amount = -doc.discount_amount
-            else:
-                df.set("print_hide", 1)
+            if df:  # avoid NoneType error
+                if doc.get("discount_amount") and hasattr(doc, "taxes") and not len(doc.taxes):
+                    df.set("print_hide", 0)
+                    doc.discount_amount = -doc.discount_amount
+                else:
+                    df.set("print_hide", 1)
 
-        # Still run ERPNext’s print setup
         from erpnext.controllers.print_settings import (
             set_print_templates_for_item_table,
             set_print_templates_for_taxes,
